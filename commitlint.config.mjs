8b5e7e0d2f7db6746ed2ccd5ba35cb1d78e84c29
LMX <2649078304@qq.com>
2026-06-06 07:07:13 +0800

    chore: format project metadata

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,19 +1,7 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [
-      2,
-      "always",
-      [
-        "main",
-        "preload",
-        "renderer",
-        "ui",
-        "deps",
-        "config",
-        "release",
-      ],
-    ],
+    "scope-enum": [2, "always", ["main", "preload", "renderer", "ui", "deps", "config", "release"]],
     "scope-case": [2, "always", "lower-case"],
   },
 };
